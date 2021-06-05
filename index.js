8292520514cfafafd6901b409bc1df84db8ed3da
nerwind93 <nerwindgd@gmail.com>
2021-06-05 11:02:09 +0500

    penis

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ class BrainfuckCompilerError extends Error {
 function brainfuck(input) {
     if (/[^+\-<>\[\],.]/.test(input)) throw new BrainfuckCompilerError('Unexpected character "' + input.match(/[^+\-<>\[\],.]/)[0] + '"');
     const prompt = require('prompt-sync')();
-    const replaced = input.replace(/\[/g, 'while (arr[i]) {\n').replace(/(?<!i)\]/g, '\n}').replace(/\+/g, 'arr[i]++;\n').replace(/-/g, 'arr[i]--;\n').replace(/\./g, 'console.log(arr[i]);\n').replace(/</g, 'i--;\n').replace(/\>/g, 'i++;\n').replace(/,/g, 'arr[i] = prompt();\n'); //what the fuck is this
-    console.log(replaced);
+    const replaced = input.replace(/\[/g, 'while (arr[i]) {\n').replace(/(?<!i)\]/g, '\n}').replace(/\+/g, 'arr[i]++;\n').replace(/-/g, 'arr[i]--;\n').replace(/\./g, 'console.log(arr[i]);\nretArr.push(String.fromCharCode(arr[i]));\n').replace(/</g, 'i--;\n').replace(/\>/g, 'i++;\n').replace(/,/g, 'arr[i] = prompt();\n'); //what the fuck is this
     let toEval = new Function('x, prompt', `
     let i = 0;
     const arr = [];
+    const retArr = [];
     for (let xo = 0, a = 0; arr.length <= 30000; a++) {
         arr[a] = xo;
     }
     ${replaced}
+    return retArr.join('');
     `);
-    console.log(toEval.toString());
     return toEval(input, prompt);
 }
 
